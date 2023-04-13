c46d880a8620a3bcf8abfb46f5621a6e6c30b0a0
Maggie Du <maggie.du@sas.com>
2023-04-13 10:47:03 -0400

    module2_2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import shutil
 
 class Parser:
-    extensions: List(str) = []
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
         return extension in self.extensions
@@ -16,18 +16,17 @@ class Parser:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-class ResourceParse(Parser):
+class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
-
 
 
